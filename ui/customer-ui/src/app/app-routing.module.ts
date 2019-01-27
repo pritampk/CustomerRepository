6b073a3c80fcf3e5117f0e4e1fed0ea94ff49f6f
rajat <rajatsaxena120@gmail.com>
2019-01-27 18:37:37 +0530

    Added table component on first route

--- ui/customer-ui/src/app/app-routing.module.ts
+++ ui/customer-ui/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { TableComponent } from "../app/common/table-component/table.component";
+const routes: Routes = [{ path: "", component: TableComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
