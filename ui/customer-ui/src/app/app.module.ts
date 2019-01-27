1d06aa283455ef3ad9461fae4d7b535372b9c2ca
rajat <rajatsaxena120@gmail.com>
2019-01-27 20:54:40 +0530

    Added material component with table

--- ui/customer-ui/src/app/app.module.ts
+++ ui/customer-ui/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-
+import { MatTableModule } from "@angular/material/table";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { TableComponent } from "../app/common/table-component/table.component";
 
 @NgModule({
   declarations: [AppComponent, TableComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- ui/customer-ui/src/app/common/table-component/table.component.ts
+++ ui/customer-ui/src/app/common/table-component/table.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  selector: "app-table",
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.css"]
 })
 export class TableComponent {
- 
+  displayedColumns: string[] = ["id", "name", "age", "gender", "address"];
+  dataSource = [
+    {
+      id: 1,
+      name: "Rajat",
+      age: 25,
+      gender: "Male",
+      address: "BTM layout 1st Stage"
+    },
+    { id: 2, name: "Pritam", age: 25, gender: "Male", address: "Roopena Agara" }
+  ];
 }
