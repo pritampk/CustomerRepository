fcf2f8d2c289507043b88c38564b70dce35cca63
pritampk <pkpritam06@gmail.com>
2019-01-30 20:46:54 +0530

    2nd commit

---
+++ customerService/src/main/java/com/pritam/entity/CustomerEntity.java
@@ -0,0 +1,52 @@
+package com.pritam.entity;
+
+public class CustomerEntity {
+
+	private int id;
+	private String name;
+	private int age;
+	private String sex;
+	private String address;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
